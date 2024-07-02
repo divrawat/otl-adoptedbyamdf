c751150fa78c98f8bc3f7834940ed175cb64f841
divrawat <divrawat2001@gmail>
2024-07-03 00:49:02 +0530

    first commit

---
+++ config.js
@@ -0,0 +1,215 @@
+
+// export const DOMAIN = 'http://localhost:3000';
+export const DOMAIN = 'https://www.adoptedbyamurderousdukefamily.in.net';
+
+export const MANGA_DESCRIPTION = `<a href="${DOMAIN}"><u>Adopted by a Murderous Duke Family</u></a> is a novel that revolves around a young woman named Agnes finds herself back in time after being exploited for her healing powers by a cruel duke. This time, she's offered adoption by Duke Rodwick, the head of a notorious family of assassins. Despite their dark reputation, Agnes accepts, hoping for a better life. However, her new family's work and her secret power put her in a precarious situation. `;
+
+export const MANGA_NAME = 'Adopted By a Murderous Duke Family';
+export const MANGA_AUTHOR = 'Not Known';
+export const MANGA_RELEASE = '2024';
+export const MANGA_STATUS = 'Ongoing';
+export const MANGA_ARTIST = 'Not Known';
+export const MANGA_STUDIO = 'None';
+export const MANGA_GENRE = 'Webtoon, Fantasy, Romance, Drama, Thriller';
+
+export const CHAPTER_PREFIX = 'adopted-by-a-murderous-duke-family-chapter';
+export const IMAGE_PREFIX = 'images/adopted-by-a-murderous-duke-family';
+export const NEXT_PREVIOUS_PREFIX = 'manga/adopted-by-a-murderous-duke-family-chapter';
+export const URL_PREFIX = 'manga/adopted-by-a-murderous-duke-family-chapter';
+
+export const AUTHOR_PAGE = `${DOMAIN}/adopted-by-a-murderous-duke-family-team`;
+export const LOGO_URL = `${DOMAIN}/logo.webp`;
+export const COVER_IMG = `${DOMAIN}/cover.webp`;
+export const BEHIND_COVER_IMG = "https://i0.wp.com/thesportsgrail.com/wp-content/uploads/2024/06/Adopted-by-a-Murderous-Duke-Family.jpg?fit=1280%2C720&ssl=1";
+
+
+// Navbar
+export const logo = <img src={`${LOGO_URL}`} alt="Logo" className="h-[75px] w-[75px] rounded-full mr-5 md:ml-0 ml-4 md:pb-2 md:my-0 my-1" />
+
+export const APP_NAME = 'Adopted By a Murderous Duke Family';
+export const NavbarName = "Adopted By MD Family";
+
+export const APP_DESCRIPTION = `Read ${MANGA_NAME} manga online at ${DOMAIN}. Agnes finds herself back in time after being exploited for her healing powers by a cruel duke. This time, she's offered adoption by Duke Rodwick, the head of a notorious family of assassins. Despite their dark reputation, Agnes accepts, hoping for a better life. However, her new family's work and her secret power put her in a precarious situation.`;
+
+export const DISQUS_SHORTNAME = "my-cms-7";
+
+
+export const MANGA_SUMMARY = [
+    {
+        id: 1,
+        content: "Adopted by a Murderous Duke Family is a compelling web novel that delves into the life of a young protagonist who finds themselves thrust into the midst of a notorious noble family renowned for their dark history and ruthless ambitions. The story begins with the protagonist, often an orphan or someone from a humble background, being adopted into the prestigious and powerful Duke family. Little do they know, this family harbors deep, sinister secrets that have shaped their reputation and power over generations."
+    },
+    {
+        id: 2,
+        content: "As the protagonist settles into their new life, they quickly realize that the Duke family is not what it seems on the surface. Beneath the facade of wealth and influence lies a complex web of intrigue, manipulation, and violence. The Dukes are not only involved in high-stakes political maneuverings but also in darker dealings such as assassinations, conspiracies, and even occult practices."
+    },
+    {
+        id: 3,
+        content: "Throughout the novel, the protagonist navigates this treacherous environment, torn between their desire for acceptance and the growing realization of the family's malevolent nature. They form alliances with various members of the Duke family, each with their own agendas and secrets, while also uncovering the truth behind past tragedies and scandals that continue to haunt the family."
+    },
+    {
+        id: 4,
+        content: "Themes of power, loyalty, betrayal, and morality are explored as the protagonist grapples with their own identity and moral compass in the face of the Duke family's influence. They must decide whether to embrace the family's legacy of darkness or to fight against it, risking their own safety and those they care about in the process."
+    },
+    {
+        id: 5,
+        content: "As the plot unfolds, tensions escalate, and the protagonist finds themselves increasingly entangled in the Duke family's schemes and conflicts. They must navigate a precarious path, balancing survival with their quest for justice and understanding, all while trying to unravel the mysteries that surround the family's rise to power and their penchant for violence."
+    },
+    {
+        id: 6,
+        content: "Adopted by a Murderous Duke Family is a gripping tale of intrigue and suspense, filled with complex characters, unexpected twists, and a richly detailed world that draws readers into a compelling narrative of ambition and treachery within the aristocracy. It challenges notions of identity and morality while offering a thrilling exploration of the lengths some will go to maintain power and control."
+    }
+];
+
+
+
+
+export const navLinks = [
+    { text: 'Home', href: `${DOMAIN}` },
+    { text: 'DMCA', href: `${DOMAIN}/dmca` },
+    { text: 'Terms & Conditions', href: `${DOMAIN}/terms-and-conditions` },
+];
+
+export const FooterLinks = [
+    { text: 'About', href: `${DOMAIN}/about` },
+    { text: 'Contact', href: `${DOMAIN}/contact` },
+    { text: 'Disclaimer', href: `${DOMAIN}/disclaimer` },
+    { text: 'Privacy Policy', href: `${DOMAIN}/privacy-policy` },
+    { text: 'Terms & Conditions', href: `${DOMAIN}/terms-and-conditions` }
+];
+
+
+
+export const RelatedMangaLinks = [
+    /*
+    {
+        imageUrl: `${DOMAIN}/related/frieran.webp`,
+        title: 'Frieran Manga',
+        link: 'https://www.frierenmanga.in.net',
+    },
+    */
+
+];
+
+
+
+
+
+export const chaptersData = [
+    {
+        "chapterNumber": "1",
+        "numImages": 92
+    },
+    {
+        "chapterNumber": "2",
+        "numImages": 91
+    },
+    {
+        "chapterNumber": "3",
+        "numImages": 86
+    },
+    {
+        "chapterNumber": "4",
+        "numImages": 94
+    },
+    {
+        "chapterNumber": "5",
+        "numImages": 15
+    },
+    {
+        "chapterNumber": "6",
+        "numImages": 14
+    },
+    {
+        "chapterNumber": "7",
+        "numImages": 14
+    },
+    {
+        "chapterNumber": "8",
+        "numImages": 12
+    },
+    {
+        "chapterNumber": "9",
+        "numImages": 15
+    },
+    {
+        "chapterNumber": "10",
+        "numImages": 12
+    },
+    {
+        "chapterNumber": "11",
+        "numImages": 14
+    },
+    {
+        "chapterNumber": "12",
+        "numImages": 13
+    },
+    {
+        "chapterNumber": "13",
+        "numImages": 14
+    },
+    {
+        "chapterNumber": "14",
+        "numImages": 14
+    },
+    {
+        "chapterNumber": "15",
+        "numImages": 13
+    },
+    {
+        "chapterNumber": "16",
+        "numImages": 13
+    },
+    {
+        "chapterNumber": "17",
+        "numImages": 11
+    },
+    {
+        "chapterNumber": "18",
+        "numImages": 11
+    },
+    {
+        "chapterNumber": "19",
+        "numImages": 13
+    },
+    {
+        "chapterNumber": "20",
+        "numImages": 12
+    },
+    {
+        "chapterNumber": "21",
+        "numImages": 9
+    },
+    {
+        "chapterNumber": "22",
+        "numImages": 10
+    },
+    {
+        "chapterNumber": "23",
+        "numImages": 11
+    },
+    {
+        "chapterNumber": "24",
+        "numImages": 14
+    },
+    {
+        "chapterNumber": "25",
+        "numImages": 10
+    },
+    {
+        "chapterNumber": "26",
+        "numImages": 12
+    },
+    {
+        "chapterNumber": "27",
+        "numImages": 8
+    },
+    {
+        "chapterNumber": "28",
+        "numImages": 13
+    },
+    {
+        "chapterNumber": "29",
+        "numImages": 9
+    }
+]

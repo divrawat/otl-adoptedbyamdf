65113f5c20c74d5ac9986782d7b6d83ed258a7bc
divrawat <divrawat2001@gmail>
2024-07-24 02:00:47 +0530

    wf0wr5f

--- pages/_app.js
+++ pages/_app.js
@@ -11,10 +11,12 @@ NProgress.configure({ showSpinner: false })
 const shantellSans = Shantell_Sans({ subsets: ['latin'], preload: false });
 
 function MyApp({ Component, pageProps }) {
+  /*
   useEffect(() => {
     const disableRightClick = (event) => { event.preventDefault(); };
     document.addEventListener('contextmenu', disableRightClick);
   }, []);
+  */
   return (
     <>
       <style jsx global>{`
